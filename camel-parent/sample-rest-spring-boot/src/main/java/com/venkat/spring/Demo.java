9b80d06a377568f2442a0ec4fa5a530c7d5ed2cf
Venkat <venkatatgithub@gmail.com>
2016-07-01 17:49:06 +0530

    Added fixes

---
+++ camel-parent/sample-rest-spring-boot/src/main/java/com/venkat/spring/Demo.java
@@ -0,0 +1,12 @@
+package com.venkat.spring;
+
+
+
+public class Demo {
+
+	public static void main(String[] args) {
+//		import org.apache.camel.component.properties.PropertiesComponent;
+//		import org.apache.camel.component.restlet.RestletComponent;		
+		
+	}
+}
